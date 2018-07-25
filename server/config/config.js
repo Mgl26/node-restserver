e0b9c7d22e9f723fd43a0be461d05e55acdf69d5
Miguel <mjsoto@entel.cl>
2018-07-25 12:17:29 -0400

    almacenando credenciales en variable de entorno para no hacerlas publicas

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:Entel.123@ds253891.mlab.com:53891/cafe';
+    // urlDB = 'mongodb://usuariosDB:pass@ds253891.mlab.com:53891/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 process.env.URLDB = urlDB;
